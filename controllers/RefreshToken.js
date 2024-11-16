6d4070008b90346208a1de6481116058a96ed6e9
SatyoAp <satyodwiapitiadi@gmail.com>
2024-11-16 13:07:51 +0700

    add dotenv in refresh token

--- controllers/RefreshToken.js
+++ controllers/RefreshToken.js
@@ -1,5 +1,7 @@
 import Users from "../model/usersModel.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const refreshToken = async (req, res) => {
   try {
