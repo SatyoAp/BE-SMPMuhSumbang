c0231ddfebbc88dd251ce0c8ada1f46e9056b08a
SatyoAp <satyodwiapitiadi@gmail.com>
2024-11-19 12:55:49 +0700

    edit info

--- controllers/Info.js
+++ controllers/Info.js
@@ -59,7 +59,7 @@ export const postInfo = async (req, res) => {
 export const putInfoById = async (req, res) => {
   try {
     const { tanggal_buka, tanggal_tutup, status, detail } = req.body;
-    if (!tanggal_buka || !tanggal_tutup || !detail) {
+    if (!tanggal_buka || !tanggal_tutup || !status || !detail) {
       return res.status(400).json({ msg: "Semua Kolom Harus Terisi" });
     }
     await updateInfoById(
@@ -69,7 +69,7 @@ export const putInfoById = async (req, res) => {
       status,
       detail
     );
-    const respon = await findGuruById(req.params.id);
+    const respon = await findInfoById(req.params.id);
     if (!respon) {
       return res.status(404).json({ msg: "data info tidak ada" });
     }
