0bd632f5e7b34be59a9214de9775c5860857fb42
SatyoAp <satyodwiapitiadi@gmail.com>
2024-11-23 16:00:01 +0700

    up

--- routes/Dokumen.js
+++ routes/Dokumen.js
@@ -7,6 +7,11 @@ import {
   deleteData,
   getDokumenById,
 } from "../controllers/Dokumen.js";
+import { fileURLToPath } from "url";
+
+// Mendefinisikan __filename dan __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const routerDok = express.Router();
 routerDok.get("/", getDokumen);
@@ -16,7 +21,7 @@ routerDok.delete("/delete/:id", deleteData);
 // Konfigurasi Multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public/images/");
+    cb(null, path.join(__dirname, "public/images"));
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
