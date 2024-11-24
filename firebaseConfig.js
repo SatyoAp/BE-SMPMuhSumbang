1060821e37db7e319795f347aa8e0cdf97838142
SatyoAp <satyodwiapitiadi@gmail.com>
2024-11-24 22:23:31 +0700

    edit service firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,24 @@
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
+// import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
 import dotenv from "dotenv";
 dotenv.config();
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // Masukkan bucket dari Firebase Console
-});
+const fireconfig = async (req, res) => {
+  try {
+    const serviceAccountKey = JSON.parse(
+      process.env.FIREBASE_SERVICE_ACCOUNT_KEY
+    );
 
-const bucket = admin.storage().bucket();
-export default bucket;
+    // Inisialisasi Firebase
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccountKey),
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    });
+
+    const bucket = await admin.storage().bucket();
+    console.log("Firebase Storage bucket initialized:", bucket.name);
+  } catch (error) {
+    console.error("Error parsing service account JSON:", error.message);
+  }
+};
+export default fireconfig;
