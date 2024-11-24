18d29081e2b12ae9300f1ee678243735ab246815
SatyoAp <satyodwiapitiadi@gmail.com>
2024-11-24 23:45:21 +0700

    update

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,11 @@
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
+// import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
 import dotenv from "dotenv";
 dotenv.config();
 
 // Parse FIREBASE_SERVICE_ACCOUNT_KEY from .env
-console.log(serviceAccount);
-// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+console.log(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 // Initialize Firebase Admin
 if (!admin.apps.length) {
