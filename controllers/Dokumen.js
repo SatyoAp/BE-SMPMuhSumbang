eb8cf77a8de1bd86856d2d2a1a8a13060b3143ce
SatyoAp <satyodwiapitiadi@gmail.com>
2024-11-26 01:15:32 +0700

    tes

--- controllers/Dokumen.js
+++ controllers/Dokumen.js
@@ -27,7 +27,7 @@ export const getDokumen = async (req, res) => {
 // Konfigurasi Multer
 const storage = multer.memoryStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/images");
+    cb(null, "public/images/");
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
