440ff5e94cc7853181784c76e3fbb4db76020b40
SatyoAp <satyodwiapitiadi@gmail.com>
2024-11-29 21:33:15 +0700

    up

--- getToken.js
+++ getToken.js
@@ -1,5 +1,7 @@
 import { google } from "googleapis";
 import readline from "readline";
+import dotenv from "dotenv";
+dotenv.config();
 
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_DRIVE_CLIENT_ID,
@@ -18,14 +20,19 @@ export const getAccessToken = async () => {
   const authUrl = oauth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
+    response_type: "code",
   });
   console.log("Authorize this app by visiting this url:", authUrl);
 
-  rl.question("Enter the code from that page here: ", (code) => {
-    oauth2Client.getToken(code, (err, token) => {
-      if (err) return console.error("Error retrieving access token", err);
-      console.log("Your refresh token:", token.refresh_token);
+  rl.question("Enter the code from that page here: ", async (code) => {
+    try {
+      const { tokens } = await oauth2Client.getToken(code);
+      console.log("Your refresh token:", tokens.refresh_token);
       rl.close();
-    });
+    } catch (err) {
+      console.error("Error retrieving access token", err);
+    }
   });
 };
+
+getAccessToken();
