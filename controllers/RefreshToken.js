ccd0ac4bff872a1cf0d078bb5a78f3c370f844ac
SatyoAp <satyodwiapitiadi@gmail.com>
2024-12-20 23:01:06 +0700

    edit refresh token

--- controllers/RefreshToken.js
+++ controllers/RefreshToken.js
@@ -7,25 +7,25 @@ export const refreshToken = async (req, res) => {
   try {
     const refreshToken = req.cookies.refreshToken;
     if (!refreshToken) return res.sendStatus(401);
-    const user = await Users.findAll({
+    const user = await Users.findOne({
       where: {
         refresh_token: refreshToken,
       },
     });
-    if (!user[0]) return res.sendStatus(403);
+    if (!user) return res.sendStatus(403);
     jwt.verify(
       refreshToken,
       process.env.REFRESH_TOKEN_SECRET,
       (err, decoded) => {
         if (err) return res.sendStatus(403);
-        const userId = user[0].id;
-        const name = user[0].name;
-        const email = user[0].email;
+        const userId = user.id;
+        const name = user.name;
+        const email = user.email;
         const accessToken = jwt.sign(
           { userId, name, email },
           process.env.ACCESS_TOKEN_SECRET,
           {
-            expiresIn: "15s",
+            expiresIn: "1d",
           }
         );
         res.json({ accessToken });
