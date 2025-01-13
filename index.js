10ba06ebe46d926d638d11dd64b646922d538136
SatyoAp <satyodwiapitiadi@gmail.com>
2025-01-13 21:15:26 +0700

    add allowedOrigins

--- index.js
+++ index.js
@@ -34,11 +34,11 @@ try {
 }
 server.use(bodyParser.json());
 server.use(
-  cors({ credentials: true, origin: "https://be-smp-muh-sumbang.vercel.app" })
+  // cors({ credentials: true, origin: "https://be-smp-muh-sumbang.vercel.app" })
 );
 // server.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
-// const allowedOrigins = ["http://192.168.1.6:5173"];
+const allowedOrigins = ["http://192.168.1.6:5173", ["http://192.168.1.6:5174"] ];
 
 const corsOptions = {
   credentials: true,
