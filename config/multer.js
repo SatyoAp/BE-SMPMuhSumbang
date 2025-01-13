5b2edd6f2978a7deca4a2d66bbf9898c229e3676
SatyoAp <satyodwiapitiadi@gmail.com>
2025-01-14 00:14:15 +0700

    update

--- config/multer.js
+++ config/multer.js
@@ -2,8 +2,12 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Pastikan direktori 'uploads/' ada
-const uploadDir = "uploads/";
+const uploadDir = path.resolve(__dirname, 'uploads');
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
