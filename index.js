093cb8d0d1fe739bd3c97b23271ada204788de85
SatyoAp <satyodwiapitiadi@gmail.com>
2025-01-13 22:40:49 +0700

    add multer

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
+import multer from "multer";
 
 // Import route
 import router from "./routes/Users.js";
@@ -20,6 +21,7 @@ dotenv.config();
 startSequelize(db);
 
 const server = express();
+const upload = multer();
 // Mendefinisikan __filename dan __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -62,8 +64,9 @@ server.use((err, req, res, next) => {
   res.status(500).json({ msg: "Internal server error", error: err.message });
 });
 
-server.use(express.json());
 server.use(bodyParser.json());
+server.use(upload.none());
+server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use("/users", router);
 server.use("/pendaftaran", pendaftaranRouter);
