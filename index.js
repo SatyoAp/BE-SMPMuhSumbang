6e1333701105e5aab969d90718ff929bd6842e87
SatyoAp <satyodwiapitiadi@gmail.com>
2025-01-13 21:19:25 +0700

    edit

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ try {
   console.error(error);
 }
 server.use(bodyParser.json());
-server.use(
-  // cors({ credentials: true, origin: "https://be-smp-muh-sumbang.vercel.app" })
-);
+// server.use(
+//   // cors({ credentials: true, origin: "https://be-smp-muh-sumbang.vercel.app" })
+// );
 // server.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 const allowedOrigins = ["http://192.168.1.6:5173", ["http://192.168.1.6:5174"] ];
